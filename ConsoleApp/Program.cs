b1a49bd341565da3a1ba7ba3c6f8775fac04055b
dd-inde <bogdanovich_7712@mail.ru>
2025-02-03 12:34:30 +0300

    initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿String[] arguments = Environment.GetCommandLineArgs();
+
+switch (arguments[1])
+{
+    case "install":
+        break;
+    case "view":
+        break;
+    case "delete":
+        break;
+    case "import":
+        break;
+    case "post":
+        break;
+}
